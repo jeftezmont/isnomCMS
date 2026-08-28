a8d44091610e6a27394c7d8629ae8cb6e907d518
Jefté Montenegro <jeftezmont@gmail.com>
2026-08-28 14:43:03 -0600

    Make podcast episodes full width

--- app/Views/layouts/site.php
+++ app/Views/layouts/site.php
@@ -17,7 +17,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="<?= asset('favicon/favicon-16x16.png') ?>">
     <link rel="shortcut icon" href="/favicon.ico">
     <link rel="apple-touch-icon" sizes="180x180" href="<?= asset('favicon/apple-touch-icon.png') ?>">
-    <link rel="stylesheet" href="<?= asset('css/site.css') ?>?v=30">
+    <link rel="stylesheet" href="<?= asset('css/site.css') ?>?v=31">
     <script defer src="<?= asset('js/site.js') ?>?v=3"></script>
     <?php
     try {
